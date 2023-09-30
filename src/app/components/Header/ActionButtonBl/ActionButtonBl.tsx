f5d513d9b71686c1e2e7d4b9d379c34bc508501c
Dmitriy <morqqulis@gmail.com>
2023-09-30 21:02:03 +0400

    removed non-unique id's

--- src/app/components/Header/ActionButtonBl/ActionButtonBl.tsx
+++ src/app/components/Header/ActionButtonBl/ActionButtonBl.tsx
@@ -11,7 +11,6 @@ const ActionButtonBl = ({ bc }: IActionButtonBl) => {
       className={`Action-btn px-[43px] py-[13px] ${
         bc == "menu" && "border-[3px] border-white"
       } hover:text-emerald-400" w-full max-w-[170px] whitespace-nowrap rounded-full bg-black text-base font-bold transition-all hover:scale-[1.1] 1250:text-white`}
-      id="contact-btn"
     >
       Contact Us
     </Link>
